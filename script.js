0c2ad244758e9afffda0a902a95e68d691dbb499
unknown <yeskennyt@gmail.com>
2022-06-06 19:39:41 +0300

    v1.0

---
+++ script.js
@@ -0,0 +1,27 @@
+a = parseInt(prompt('a'));
+console.log(a);
+while(a < 0 || isNaN(a)) {
+    alert(`a должно быть больше 0`);
+    a = parseInt(prompt('a'));
+}
+
+b = parseInt(prompt('b'));
+while (a >= b || isNaN(b)) {
+    alert(`a должно быть меньше b`)
+    b = parseInt(prompt('b'));
+} 
+
+h = parseInt(prompt('h'));
+while(h < 0 || isNaN(h)) {
+    alert(`h должно быть больше 0`);
+    h = parseInt(prompt('h'));
+}
+
+for(i = a + h, sum = 0; i < b; i += h) {
+    for (j = 1, factorial = 1; j <= i; j++){
+        factorial = factorial * j;
+    }
+    console.log(`Факториал числа ${i}! = ${factorial}`);
+    sum = sum + factorial;
+}
+console.log(`Сумма факториалов = ${sum}`);
